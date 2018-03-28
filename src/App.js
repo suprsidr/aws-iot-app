38c7bb13b4a0611b42a0255ab77e816e2861029a
Wayne Patterson <Wayne.Patterson@nextgearcapital.com>
2018-03-28 15:22:03 -0400

    logo resize

--- src/App.js
+++ src/App.js
@@ -138,13 +138,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            src={logo}
+            className={`App-logo ${this.state.className}`}
+            alt="logo"
+          />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div className={`button ${this.state.className}`} />
         <p>{this.state.action}</p>
         <p>{this.state.clientId}</p>
       </div>
